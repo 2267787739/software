7cb1725d1b6615ee4854ff6587576238cf6e8c92
yyt <47936630+2267787739@users.noreply.github.com>
2019-11-13 17:33:51 +0800

    Add files via upload

---
+++ chuti/src/visibleArithmeticExercise/ArithmeticTest8.java
@@ -0,0 +1,11 @@
+package visibleArithmeticExercise;
+
+import visibleArithmeticExercise.MyExGUI;
+
+public class ArithmeticTest8 {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        MyExGUI exercise = new MyExGUI();
+    }
+
+}
